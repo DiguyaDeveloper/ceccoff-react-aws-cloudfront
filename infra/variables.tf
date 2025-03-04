847a79d781d4324828b1b65fb02ef540b8302554
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-03-04 09:11:21 -0300

    build: :construction: Remove variables of aws connection

--- infra/variables.tf
+++ infra/variables.tf
@@ -26,18 +26,3 @@ variable "acm_certificate_arn" {
   type        = string
   default     = ""
 }
-
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-  description = "aws_access_key_id"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-  description = "aws_secret_access_key"
-}
-
-variable "AWS_SESSION_TOKEN" {
-  type = string
-  description = "aws_session_token"
-}
